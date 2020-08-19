79a2a656952277105e07222f52a51e39e24aec2d
Cyrus Tse <kaho0533@yahoo.com.hk>
2020-08-19 21:44:27 +0800

    add count

--- guess.py
+++ guess.py
@@ -2,12 +2,15 @@ import random
 
 r = random.randint(1 , 100)
 while True:
+	count += 1 
 	num = inpit('please guess the random number:')
 	num = int(num)
 	if num == r:
 		print('your answer is right')
+		print('these are your guess number', count , 'times')
 		break
-    elif num > u:
+    elif num > r:
     	print('your answer is more than original')
     elif num < r:
     	print('your answer is less than original')
+    print('these are your guess number', count , 'times')
